82de96ddf8a22156727d64b23de1dd5fea34076e
Steven Le <steven@res380d-128-61-82-27.res.gatech.edu>
2021-02-27 03:06:53 -0500

    add title and start button to welcome screen as well as improve code readibility

--- src/StartScreen.java
+++ src/StartScreen.java
@@ -1,10 +1,15 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class StartScreen extends Application {
@@ -12,12 +17,32 @@ public class StartScreen extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         StackPane top = new StackPane();
+        FlowPane center = new FlowPane();
+        top.setPadding(new Insets(50, 0, 0, 0));
+        top.setAlignment(Pos.CENTER);
+        center.setAlignment(Pos.CENTER);
+
+
+        Label title = new Label("Team 68 Dungeon Crawler");
+        title.setFont(new Font("Georgia", 50));
+        title.setAlignment(Pos.CENTER);
+        top.getChildren().add(title);
+
+        Button start = new Button("Start Game");
+        start.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("poggers");
+            }
+        });
+        start.setFont(new Font("Georgia", 25));
+        center.getChildren().add(start);
+
         BorderPane root = new BorderPane();
-        Label label = new Label("pog");
-        label.setAlignment(Pos.CENTER);
-        top.getChildren().add(label);
         root.setTop(top);
-        Scene scene = new Scene(root, 1000, 800);
+        root.setCenter(center);
+
+        Scene scene = new Scene(root, 800, 450);
         stage.setScene(scene);
         stage.setTitle("Dungeon Crawler");
         stage.show();
